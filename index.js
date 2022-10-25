dd3a042ff1a3ee2a69a7f4ed61f57f9224a00151
Nayon <nayonroy64@gmail.com>
2022-10-25 23:18:43 +0600

    add courses.json file for courses fake data

--- index.js
+++ index.js
@@ -2,11 +2,22 @@ const express = require('express')
 const app = express()
 const cors = require("cors")
 
+
 // select port 
 const port = process.env.PROT || 5000
 
+
 // app use cors middleware
 app.use(cors())
+
+
+// select courses data
+const courses = require("./data/courses.json")
+
+// response courses data in /courses api
+app.get("/courses", (req, res) => {
+	res.send(courses)
+})
 
 
 app.listen(port, () => {
